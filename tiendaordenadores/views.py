514455bad0dbab3371895533ee5a2602f61f42ba
Israel Hernandez <israel.hernandez.chacon.al@iespoligonosur.org>
2025-01-24 14:37:38 +0100

    terminar en casa

--- tiendaordenadores/views.py
+++ tiendaordenadores/views.py
@@ -4,8 +4,9 @@ import requests
 
 def procesadores_lista_api(request):
     #Obtenemos todos los procesadores
-    headers = {'Authorization': 'Bearer Mgoh614AcVkUfAT8f1BZSxBonRTFI0'}
+    headers = {'Authorization': 'Bearer     '}
     response = requests.get('http://127.0.0.1:8000/api/v1/procesadores', headers = headers)
     #Transformamos la respuesta en json
     procesadores = response.json()
     return render(request, 'template-api/procesador_list.html',{"procesadores_mostrar":procesadores})
+
